a5142df2ff323b522511e9312a577a1ab8a610c2
DrQuadCore <kyhoon@gmail.com>
2019-09-16 10:35:01 +0900

    Add files via upload

---
+++ demo/lecture03/structured_binding.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int a[2] = {1,2};
+  auto [x,y] = a;
+  auto& [xr, yr] = a;
+  cout << x << "," << y << endl;
+
+  xr = 3;
+  yr = 4;
+
+  // what will be the result?
+  //cout << x << "," << y << endl;
+  //cout << a[0] << "," << a[1] << endl;
+  //cout << xr << "," << yr << endl;
+
+  struct {
+    int i=1;
+    double d=2;
+  } f;
+  auto [i,d] = f;
+  cout << i << " " << d << endl;
+
+  return 0;
+}
