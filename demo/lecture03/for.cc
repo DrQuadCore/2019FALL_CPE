f1fffa2978a1cc9b6f9f2da53913a6dd62041cc5
DrQuadCore <kyhoon@gmail.com>
2019-09-17 08:45:57 +0900

    Update for.cc

--- demo/lecture03/for.cc
+++ demo/lecture03/for.cc
@@ -6,7 +6,7 @@ int main()
 {
   int arr[] = {20, 30, 40, 50};
 
-  for(int i=0; i<sizeof(arr)/sizeof(int); i++) { // messy :(
+  for(int i=0; i<sizeof(arr)/sizeof(int); i++) // messy :(
     cout << arr[i] << " ";
   cout << endl;
 
